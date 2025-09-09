f89fc64e1cf143a5a480b6c102adb6814616d985
Yijun <buglittlej@gmail.com>
2025-09-09 20:53:38 +0800

    Constant pool research.

--- src/main/java/com/avaya/jvm/example/HelloWorld.java
+++ src/main/java/com/avaya/jvm/example/HelloWorld.java
@@ -5,6 +5,9 @@ public class HelloWorld {
     private int para = 10;
 
     public static void main(String[] args) {
-        System.out.println("Test");
+
+        String s1 = "test";
+        String s2 = new String("test");
+        System.out.println(s1==s2);
     }
 }
